dc61ce70176e5945e0694fbf75fb7a9987e99d16
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 19:58:25 +0430

    init

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <Dashboard></Dashboard>
+      <Route>
+        <Dashboard></Dashboard>
+      </Route>
       <Login />
-      <Error />
     </Router>
   );
 }
