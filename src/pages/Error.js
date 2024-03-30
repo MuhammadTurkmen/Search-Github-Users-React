3db57d16cb0eff5cbaf3f551a2262febe5bb5527
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 20:50:57 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+    </div>
+  </Wrapper>
 };
 const Wrapper = styled.section`
   min-height: 100vh;
