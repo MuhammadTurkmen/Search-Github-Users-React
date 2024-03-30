5f9d85fd45355ee5984f064663dd19e22c27bcbd
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 20:24:43 +0430

    init

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <Route path="/" exact={true}>
-        <Dashboard></Dashboard>
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route>
-        <Error path="*"/>
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route>
+          <Error path="*"/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
