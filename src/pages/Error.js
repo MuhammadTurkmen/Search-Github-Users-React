9d3c84a0b7ff2eef174ee9e86870f3557a6478d4
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 20:53:59 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,6 +6,7 @@ const Error = () => {
     <div>
       <h1>404</h1>
       <h3>sorry, the page you tried cannot be found</h3>
+      <Link to="/" className="btn">back home</Link>
     </div>
   </Wrapper>
 };
