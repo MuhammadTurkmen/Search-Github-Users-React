fcc148f1c64d8273c4834eaa060389efc0a5ffb6
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 11:03:16 +0430

    init

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,6 +5,8 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
     const {repos} = React.useContext(GithubContext)
 
+    let languages
+
     const chartData = [
     {
       label: "HTML",
