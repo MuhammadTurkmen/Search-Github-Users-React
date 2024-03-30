a875133ad09d7f48fcb84e15f46243b541e0e224
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-30 21:22:15 +0430

    init

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Card from './Card';
 import Followers from './Followers';
 const User = () => {
-  return <h2>search component</h2>;
+  return <section className='section'>search component</section>;
 };
 
 const Wrapper = styled.div`
