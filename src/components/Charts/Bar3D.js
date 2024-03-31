8e42d0a4c4c08291467b7850589ce3874f1f40fd
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 14:19:09 +0430

    init

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -25,7 +25,7 @@ ReactFC.fcRoot(FusionCharts, chart, FusionTheme);
 
 const ChartComponent  = ({data}) => {
   const chartConfigs = {
-  type: "column3d", // The chart type
+  type: "bar3d", // The chart type
   width: "100%", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
