905e237d3a83e9ed466f41481fbc4441a291e53b
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 12:15:02 +0430

    init

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -25,7 +25,7 @@ ReactFC.fcRoot(FusionCharts, chart, FusionTheme);
 
 const ChartComponent  = ({data}) => {
   const chartConfigs = {
-  type: "pie3d", // The chart type
+  type: "doughnut2d", // The chart type
   width: "400", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
