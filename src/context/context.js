c8cf21427fb857c37fa9944067c546e623ff2df0
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 10:46:20 +0430

    init

--- src/context/context.js
+++ src/context/context.js
@@ -22,7 +22,7 @@ const GithubProvider = ({children}) => {
  
     const searchGithubUser = async (user) => {
         toggleError()
-        setoading(true)
+        setIsLoading(true)
         const response = await axios(`${rootUrl}/users/${user}`).catch(err => console.log(err))
         if(response) {
             setGithubUser(response.data)
