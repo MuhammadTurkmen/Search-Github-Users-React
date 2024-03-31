86f691365bac51144a8ff9f7494d9b4ddb8c1985
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 14:20:26 +0430

    init

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -6,11 +6,15 @@ const Followers = () => {
   const  {followers} = React.useContext(GithubContext)
 
   return <Wrapper>
-    <div className='flollowers'>
+    <div className='followers'>
       {followers.map((follower, index) => {
         const {avatar_url:img, html_url, login} = follower
         return <article key={index}>
           <img src={img} alt={login}/>
+          <div>
+            <h4>{login}</h4>
+            <a href={html_url}>{html_url}</a>
+          </div>
         </article>
       })}
     </div>
