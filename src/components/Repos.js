9318d0d9180ef3a3651c369ee04ede1060f76062
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 12:09:43 +0430

    init

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -11,7 +11,7 @@ const Repos = () => {
       if(!total[language]) {
         total[language] = {label: language, value: 1}
       } else {
-        total[language] = {...total[language], value: total[language.value + 1]}
+        total[language] = {...total[language], value: total[language].value + 1}
       }
       return total
     }, {}) 
