5e9832b32d8a5ee27d3badeaae10df1e97b1fd82
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 16:06:20 +0430

    init

--- src/context/context.js
+++ src/context/context.js
@@ -30,7 +30,14 @@ const GithubProvider = ({children}) => {
 
             await Promise.allSettled([ axios(`${rootUrl}/users/${login}/repos?per_page=100`),
             axios(`${followers_url}?per_page=100`) ]).then((results) => {
-                console.log(results);
+                const [repos, followers] = results
+                const status = 'fulfilled'
+                if(repos.status === status) {
+                    setRepos(repos.value.data)
+                }
+                if(followers.status === status) {
+                    setFollowers(followers.value.data)
+                }
             })
         }
         else {
