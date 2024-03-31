58089dc1940af30d53342ba296769532fbf4b8d1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 10:41:46 +0430

    init

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('')
-  const {requests, error, searchGithubUser} = React.useContext(GithubContext)
+  const {requests, error, searchGithubUser, isLoading} = React.useContext(GithubContext)
   // get things from globle context
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -27,7 +27,7 @@ const Search = () => {
           <MdSearch />
           <input type='text' placeholder='enter github user' value={user} onChange={(e) => setUser(e.target.value)}/>
 
-          {requests > 0 && <button type='submit'>search</button>}
+          {requests > 0 && !isLoading && <button type='submit'>search</button>}
         </div>
       </form>
       <h3>requests : {requests} / 60</h3>
