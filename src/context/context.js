06487716463130b24274966e20985d96f967864e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-31 10:50:53 +0430

    init

--- src/context/context.js
+++ src/context/context.js
@@ -27,6 +27,7 @@ const GithubProvider = ({children}) => {
         if(response) {
             setGithubUser(response.data)
             //  more logic here 
+            // repose
         }
         else {
             toggleError(true, 'there is no user with that usernames')
