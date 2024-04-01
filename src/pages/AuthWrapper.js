f77bbb9e26dea0d07eb602dd71e8a3e0030e43de
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-01 14:18:27 +0430

    init

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import loadingGif from '../images/preloader.gif';
 import styled from 'styled-components';
-function AuthWrapper() {
+function AuthWrapper({children}) {
   const {isLoading, error} = useAuth0()
   if(isLoading) {
 
   }
 
   if(error) {
-    
+
   }
-  return <h2>authwrapper component</h2>;
+  return <>{children}</>;
 }
 
 const Wrapper = styled.section`
