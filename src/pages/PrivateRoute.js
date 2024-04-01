a73714549d3081aff69e332a4d5717044f87f27a
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-01 15:56:39 +0430

    init

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = () => {
+  const {isAuthenticated, user} = useAuth0()
   return <h2>Private Route</h2>
 };
 export default PrivateRoute;
