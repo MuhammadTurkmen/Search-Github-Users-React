e564cdda2eb436e2fd87183ae5aa82066e38db40
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-01 10:50:01 +0430

    init

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,7 +3,8 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({children, ...rest}) => {
-  const isUser = true
+  const {isAuthenticate, user} = useAuth0()
+  const isUser = false
   return <Route {...rest} render={() => {
     return isUser ? children : <Redirect to='login'></Redirect>
   }} ></Route >;
